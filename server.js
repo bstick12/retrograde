257bfa87b88199d34fc679d450609253a2c0b324
Brendan Nolan <brendan.nolan@gmail.com>
2018-02-06 23:37:04 +0000

    Add bootstrap

--- server.js
+++ server.js
@@ -21,8 +21,10 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
-app.use('/highcharts', express.static(__dirname + '/node_modules/highcharts/dist'));
+app.use('/highcharts', express.static(__dirname + '/node_modules/highcharts'));
 app.use('/bootswatch', express.static(__dirname + '/node_modules/bootswatch/dist'));
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'));
+app.use('/popper.js', express.static(__dirname + '/node_modules/popper.js/dist'));
 
 var routes = require('./api/routes/retroRoutes.js');
 routes(app)
